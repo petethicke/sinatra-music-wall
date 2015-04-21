4bfb5f9504451102b42aca6d5a0b9135be16a5ba
Peter Thicke <peterbthicke@gmail.com>
2015-04-21 21:35:04 +0000

    added some login information

--- db/migrate/20150421033210_add_users.rb
+++ db/migrate/20150421033210_add_users.rb
@@ -2,7 +2,8 @@ class AddUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
+      t.string :password
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420212011) do
+ActiveRecord::Schema.define(version: 20150421033210) do
 
   create_table "songs", force: true do |t|
     t.string   "songtitle",  null: false
     t.string   "author",     null: false
     t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
