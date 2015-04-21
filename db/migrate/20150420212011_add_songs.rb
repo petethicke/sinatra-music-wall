29ffc7cf4ab8ea964efecb2642988cb449412cff
Peter Thicke <peterbthicke@gmail.com>
2015-04-21 04:15:43 +0000

    started implementing user authentication

--- db/migrate/20150420212011_add_songs.rb
+++ db/migrate/20150420212011_add_songs.rb
@@ -1,6 +1,7 @@
 class AddSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
+      t.belongs_to :user, index: true
       t.string :songtitle, :null => false
       t.string :author, :null => false
       t.string :url
